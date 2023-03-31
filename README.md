c1df1d85f9a457ad662a2a23574ec36c8feb8747
Mannavakeerthana <94091976+Mannavakeerthana@users.noreply.github.com>
2023-03-31 07:52:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,25 +1,21 @@
-# Bloglist frontend
+# Solutions of part 1 exercises
 
-In this exercise, we will now create a frontend for the bloglist backend we created in the last part.
-A login functionnality is also implemented for restricting the possibility to view and create blogs only by authenticated users. We assume that a user already exists with the good credentials.
+In this part, we will familiarize ourselves with the React-library, which we will be using to write the code that runs in the browser. We will also look at some features of Javascript that are important for understanding React.
 
-Since the objective of this part is to test the react app, unit tests and end-to-end (E2E) tests with cypress are also implemented.
+## Requirements
+* [node](https://nodejs.org/en/download/)
+* [yarn](https://classic.yarnpkg.com/en/docs/install/#debian-stable)
 
-## Start the application locally
 
-To start an application:
+## Start the application
+
+There is one application by folder, to start an application :
 
 ```bash
-# First, you need to start the backend, to do so, head to the part4. Everything is explained in the README
-
-# Install dependancies
-$ npm install
-
-# Start the frontend application
-$ npm start
-
-# For running E2E tests
-$ npm run cypress:open        # Then, click on run all specs
+# Head to the desired exercise (courseinfo, unicafe or anecdotes)
+$ cd courseinfo
+# Start the application
+$ yarn start
 ```
 
-You can then access the app on : http://localhost:3000/
+You can then access the frontend on : [http://localhost:3000/](http://localhost:3000/)
--- index.js
+++ index.js
@@ -1,20 +1,85 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-// Import commands.js using ES2015 syntax:
-import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+const Header = props => <h1>{props.name}</h1>
+
+const Statistic = ({text, value}) => {
+  if (text === "positive") {
+    return (
+      <tr><td>{text} {value} %</td></tr>
+    )
+  }
+
+  return (
+    <tr><td>{text} {value}</td></tr>
+  )
+}
+
+const Statistics = ({clicks}) => {
+    const total = clicks.good + clicks.neutral + clicks.bad
+    const average = (clicks.good * 1 + clicks.bad * -1) / total
+    const positive = clicks.good * (100/total)
+
+    if (total === 0) {
+      return (
+        <div>
+          No feedback given
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <table>
+          <tbody>
+            <Statistic text="good" value={clicks.good} />
+            <Statistic text="neutral" value={clicks.neutral} />
+            <Statistic text="bad" value={clicks.bad} />
+            <Statistic text="all" value={total} />
+            <Statistic text="average" value={average} />
+            <Statistic text="positive" value={positive} />
+          </tbody>
+        </table>
+      </div>
+    )
+}
+
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const handleGoodClick = () =>
+    setClicks({...clicks, good: clicks.good + 1})
+
+  const handleNeutralClick = () =>
+    setClicks({...clicks, neutral: clicks.neutral + 1})
+
+  const handleBadClick = () =>
+    setClicks({...clicks, bad: clicks.bad + 1})
+
+
+  return (
+    <div>
+      <Header name="Customer feedback" />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Header name="Statistics" />
+      <Statistics clicks={clicks} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
